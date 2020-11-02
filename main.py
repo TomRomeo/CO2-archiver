5f3a5d654718f18cb5864d31c40bbacecd8ba977
Tom Romeo <Tom.Romeo.Doil@gmail.com>
2020-11-02 11:43:30 +0100

    basic .env skeleton
    added a rule to .gitignore to ignore .env files
    added a python dependency to the README
    added a basic main.py, that reads the necessary environment variables

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+# load the .env file into local environment variables
+load_dotenv(join(dirname(__file__), ".env"))
+
+
+# populate the variables with the environment variable values
+ip = os.getenv("IP")
+port = os.getenv("PORT")
+
+print(ip)
+print(port)
